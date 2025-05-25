32b4c98e7d8dd0e0f4255a9df0c80f52ad119a85
subhamonsey <subhamonsupantha@gmail.com>
2025-05-25 21:35:18 +0530

    Update installer

--- run_downloader.bat
+++ run_downloader.bat
@@ -3,6 +3,6 @@ echo ==== Moodle PDF Downloader ====
 set /p LOGIN_URL=Enter Moodle login URL:
 set /p COURSE_URL=Enter course URL:
 
-download_moodle_pdfs.exe --login-url %LOGIN_URL% --course-url %COURSE_URL%
+download_moodle_resources.exe --login-url %LOGIN_URL% --course-url %COURSE_URL%
 
 pause
